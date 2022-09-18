ee69532823073efb461352fd7db8c963298e0f72
Angelika <anzhelika48@mail.ru>
2022-09-18 20:33:57 +0500

    Решила задачи

---
+++ Zadacha_64/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+Console.WriteLine("Введите число m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n");
+int n= Convert.ToInt32(Console.ReadLine());
+
+
+string Natural (int m, int n)
+{
+    if (m > n)
+    {
+        if (m==n) return Convert.ToString(m);
+        return m + " " + Natural(m-1, n);
+    }
+    else
+    {
+      if (m==n) return Convert.ToString(m);
+       return m + " " + Natural(m+1, n);  
+    }
+    
+}
+
+ Console.WriteLine(Natural(m, n));
---
+++ Zadacha_66/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.WriteLine("Введите число m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = GetSum(m, n);
+
+int GetSum (int m, int n) 
+{
+    int sum = 0;
+     if (m > n) 
+    {
+        return sum;
+    }
+    else
+    {
+        return sum  = m + GetSum(m + 1, n); 
+    }
+}
+Console.WriteLine(sum);
